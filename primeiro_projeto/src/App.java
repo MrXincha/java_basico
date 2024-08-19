28a2f84a57f0c9d4d0eefdac0bb2deb327786c30
Lucas Mateus <lucas.mateusdev@gmail.com>
2024-08-18 21:03:56 -0300

    Correçao

--- primeiro_projeto/src/App.java
+++ primeiro_projeto/src/App.java
@@ -1,6 +1,4 @@
 public class App {
-    git config --global user.email "lucas.mateusdev@gmail.com"
-    git config --global user.name "Lucas Mateus"
     public static void main(String[] args) throws Exception {
         System.out.println("Fala galerinha!");
     }
