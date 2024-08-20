c5e0599243e8f27be36280ac594b7ecbd2448cba
Lucas Mateus <lucas.mateusdev@gmail.com>
2024-08-19 21:57:01 -0300

    v1

---
+++ java_boletim_escolar/Boletim.java
@@ -0,0 +1,12 @@
+public class Boletim {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if(mediaFinal < 6)
+        System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+        System.out.println("Refaça a prova");
+        else if (mediaFinal >= 7)
+        System.out.println("Aprovado");
+
+    }
+}
